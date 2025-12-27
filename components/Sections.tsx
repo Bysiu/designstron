109a2a9d3adb8a02e9f55393e4b848ad525a9a80
Bysiu <bysiewicz.janek@gmail.com>
2025-12-27 16:41:30 +0100

    Update Sections.tsx

--- components/Sections.tsx
+++ components/Sections.tsx
@@ -12,6 +12,7 @@ export default function DesignStronLanding() {
     name: '',
     email: '',
     phone: '',
+    subject: '',
     socials: '',
     message: ''
   });
@@ -46,6 +47,7 @@ export default function DesignStronLanding() {
       name: '',
       email: '',
       phone: '',
+      subject: '',
       socials: '',
       message: ''
     });
